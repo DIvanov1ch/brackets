9d44a5bf34d2413d778bc01376eb8c122f82a1b7
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2023-01-14 16:27:41 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = bracketsConfig
+    .reduce((array, value) => array
+      .concat(value.join('')), []);
+  for (let i = 0; i < brackets.length; i++) {
+    let currentValue = brackets[i];
+    while (str.includes(currentValue)) {
+      str = str.replace(currentValue, '');
+      i = -1;
+    }
+  }
+  return str.length == 0;
 }
